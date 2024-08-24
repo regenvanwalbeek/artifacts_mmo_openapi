d40a9cb05d292a87fce5bb93da8ab3d230f18cd9
Regen Van Walbeek <regenvanwalbeek@gmail.com>
2024-08-24 12:33:47 -0500

    Manually fix generated output from ValidationErrorLocInner

--- lib/artifacts_mmo_openapi_generator.dart
+++ lib/artifacts_mmo_openapi_generator.dart
@@ -1,4 +1,4 @@
-// Openapi Generator last run: : 2024-08-24T12:31:22.505699
+// Openapi Generator last run: : 2024-08-24T12:33:12.063116
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 // dart run build_runner build --delete-conflicting-outputs
