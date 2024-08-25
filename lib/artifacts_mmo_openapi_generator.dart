2faece66b9bfe0306f6b446672c62b108d1fdbda
Regen Van Walbeek <regenvanwalbeek@gmail.com>
2024-08-25 07:46:21 -0500

    Update pub, remove tests

--- lib/artifacts_mmo_openapi_generator.dart
+++ lib/artifacts_mmo_openapi_generator.dart
@@ -1,13 +1,17 @@
-// Openapi Generator last run: : 2024-08-24T12:33:12.063116
+// Openapi Generator last run: : 2024-08-25T07:44:10.894869
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 // dart run build_runner build --delete-conflicting-outputs
 @Openapi(
   additionalProperties: AdditionalProperties(
     pubName: 'artifacts_mmo_openapi',
+    pubDescription: 'Generated client for ArtifactsMMO',
+    pubHomepage: 'https://github.com/regenvanwalbeek/artifacts_mmo_openapi',
+    pubVersion: "1.0.0",
   ),
   inputSpec: RemoteSpec(path: 'https://api.artifactsmmo.com/openapi.json'),
   generatorName: Generator.dart,
   outputDirectory: 'artifacts_mmo_openapi',
+  runSourceGenOnOutput: true,
 )
 class ArtifactsMMO {}
