161d562fd8f3cbb035ee1d9a8bf5d24c3ce1358b
Regen Van Walbeek <regenvanwalbeek@gmail.com>
2026-02-22 09:32:32 -0600

    Re-apply simulation fix

--- lib/model/combat_result_schema.dart
+++ lib/model/combat_result_schema.dart
@@ -87,9 +87,17 @@ class CombatResultSchema {
         logs: json[r'logs'] is Iterable
             ? (json[r'logs'] as Iterable).cast<String>().toList(growable: false)
             : const [],
-        characterResults: listFromJson(json[r'character_results']),
+        characterResults: listObjectFromJson(json[r'character_results']!)!,
       );
     }
+    return null;
+  }
+
+  static List<Object>? listObjectFromJson(dynamic json) {
+    if (json is List) {
+      return json.nonNulls.map((e) => e).toList();
+    }
+
     return null;
   }
 
